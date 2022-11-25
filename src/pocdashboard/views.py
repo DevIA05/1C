71d8f31fc961b94c92c1f89fe0de6ee3503688e9
DevIA05 <dia05@polenumerique.re>
2022-11-26 01:02:49 +0400

    mise en place de la page de connexion

---
+++ src/pocdashboard/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, logout
+from django.contrib import messages
+
+# Create your views here.
+def login_user(request):
+    if request.method == "POST":
+        matricule = request.POST['matricule']
+        password = request.POST['password']
+        user = authenticate(request, matricule=matricule, password=password)
+        if user is not None:
+            # next page
+            pass
+        else:
+            messages.success(request, ("Erreur d'identifiant ou de mot de passe, veuillez ressayer"))
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+    
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("Session deconnectée"))
+    return redirect('login')
+
+def p1(request):
+    return render(request, 'p1.html')
