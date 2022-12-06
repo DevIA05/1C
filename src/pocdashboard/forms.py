7c532da7c4938aad37dae261877ed43b4b57b9c9
DevIA05 <dia05@polenumerique.re>
2022-12-06 16:06:02 +0400

    update du form dans le but d'ajouter un style

--- src/pocdashboard/forms.py
+++ src/pocdashboard/forms.py
@@ -5,4 +5,10 @@ def validate_file_extension(value):
             raise forms.ValidationError("Seul les fichiers CSV sont acceptés")
 
 class CsvImportForm(forms.Form):
-    csv_upload = forms.FileField(label='',validators=[validate_file_extension]) #widget=forms.FileInput(attrs={'accept': ".csv"}))
+    csv_upload = forms.FileField(label='',validators=[validate_file_extension],
+    widget=forms.FileInput(attrs={'accept': ".csv",
+                                  'class' : "inputfile inputfile-1",
+                                   'data-multiple-caption': "{count} files selected",
+                                   'multiple': "",
+                                   'name': "file-1[]",
+                                   'id' : "file-1"}))
