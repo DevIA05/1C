2af4266bc5901c204690025e387b3ab04b79826f
DevIA05 <dia05@polenumerique.re>
2022-12-11 15:36:00 +0400

    correction ne change pas les attributs du bouton lorsqu'il est désactivé

--- src/pocdashboard/static/js/stopAnimation.js
+++ src/pocdashboard/static/js/stopAnimation.js
@@ -1,13 +1,15 @@
 const dwf = document.getElementById("dwf");
-dwf.addEventListener('mouseover', mh)
-dwf.addEventListener('mouseout', mo)
+if(!dwf.disabled){
+    dwf.addEventListener('mouseover', mh)
+    dwf.addEventListener('mouseout', mo)
 
-function mh(e){
-    dwf.style.animation = "pause";
-    dwf.style.backgroundColor = "#277813";
-    dwf.style.transform = "scale(1)";
-};
+    function mh(e){
+        dwf.style.animation = "pause";
+        dwf.style.backgroundColor = "#277813";
+        dwf.style.transform = "scale(1)";
+    };
 
-function mo(e){
-    dwf.style.backgroundColor = "#3cb371"
+    function mo(e){
+        dwf.style.backgroundColor = "#3cb371"
+    }
 }
