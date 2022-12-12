a3c8a5c9e7c1ef4df0d4e22f76c882ad3344bbb2
DevIA05 <dia05@polenumerique.re>
2022-12-12 16:09:19 +0400

    mise à jour du model de BD

--- src/pocdashboard/models.py
+++ src/pocdashboard/models.py
@@ -3,18 +3,19 @@
 #   * Rearrange models' order
 #   * Make sure each model has one field with primary_key=True
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `` lines if you wish to allow Django to create, modify, and delete the table
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
 
 class Country(models.Model):
+    # country_id = models.AutoField(primary_key=True)
     country_name = models.CharField(primary_key=True, max_length=50)
     zone_name = models.CharField(max_length=50, blank=True, null=True)
 
     class Meta:
-        
-        db_table = 'Country'
+
+        db_table = 'country'
 
 
 class Detailfacture(models.Model):
@@ -25,19 +26,19 @@ class Detailfacture(models.Model):
     detailfacture_id = models.AutoField(primary_key=True)
 
     class Meta:
-        
-        db_table = 'Detailfacture'
+        unique_together = ('stock_code', 'invoice_no')
+        db_table = 'detailfacture'
 
 
 class Invoice(models.Model):
     invoice_no = models.CharField(primary_key=True, max_length=6)
-    invoice_date = models.DateTimeField(blank=True, null=True)
-    country_name = models.ForeignKey(Country, models.DO_NOTHING, db_column='country_name')
+    invoice_date = models.CharField(max_length=20, blank=True, null=True)
+    country_name = models.ForeignKey(Country, models.DO_NOTHING, db_column='country_id')
     customer_id = models.CharField(max_length=5, blank=True, null=True)
 
     class Meta:
-        
-        db_table = 'Invoice'
+
+        db_table = 'invoice'
 
 
 class Product(models.Model):
@@ -45,5 +46,5 @@ class Product(models.Model):
     description = models.CharField(max_length=80, blank=True, null=True)
 
     class Meta:
-        
-        db_table = 'Product'
+
+        db_table = 'product'
