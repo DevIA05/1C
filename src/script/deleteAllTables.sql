4fd9a28b008d406ed63f2708d505dd75adcaab8f
DevIA05 <dia05@polenumerique.re>
2022-12-13 14:16:02 +0400

    save

--- src/script/deleteAllTables.sql
+++ src/script/deleteAllTables.sql
@@ -1,16 +1,15 @@
 DROP TABLE
-"Country",
-"Detailfacture",
-"Invoice",
-"Product",
 "auth_group",
 "auth_group_permissions",
 "auth_permission",
 "auth_user",
 "auth_user_groups",
 "auth_user_user_permissions",
+"country",
 "detailfacture",
 "django_admin_log",
 "django_content_type",
 "django_migrations",
-"django_session";
+"django_session",
+"invoice",
+"product"
--- src/script/getNameTables.sql
+++ src/script/getNameTables.sql
@@ -2,3 +2,18 @@ SELECT table_name
 FROM information_schema.tables
 WHERE table_schema = 'public'
 ORDER BY table_name;
+
+-- "auth_group",
+-- "auth_group_permissions",
+-- "auth_permission",
+-- "auth_user",
+-- "auth_user_groups",
+-- "auth_user_user_permissions",
+-- "country",
+-- "detailfacture",
+-- "django_admin_log",
+-- "django_content_type",
+-- "django_migrations",
+-- "django_session",
+-- "invoice",
+-- "product"
