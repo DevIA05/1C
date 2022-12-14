ea30ccc0e53b1b48605e9d53934130dc1a26cbc4
DevIA05 <dia05@polenumerique.re>
2022-12-14 22:53:48 +0400

    mise en place de la demande et la réception des données dans le but de créer un graphique

--- src/pocdashboard/static/js/getDataForGraphic.js
+++ src/pocdashboard/static/js/getDataForGraphic.js
@@ -1,11 +1,30 @@
-let btns = document.querySelectorAll('callG');
+let btns = document.querySelectorAll(".callG");
 
-for (i of btns) {
-    i.addEventListener('click', function() {
-      console.log(this);
+for (b of btns) {
+    b.addEventListener('click', function() {
+      dataRequest(this.name)
     });
 }
 
 
-console.log("-------------------------------")
+function dataRequest(name_button){
+  const csrf  = $('input[name="csrfmiddlewaretoken"]').val()   // collect token
+  // ------------------- Send data to view -------------------
+  $.ajax({
+      type: "POST",
+      url: 'getDataForChart', // Name of the django view that will retrieve the data
+      data: {
+          csrfmiddlewaretoken : csrf,
+          "result": name_button,       // data to send
+      },
+      dataType: "json",
+      // ------------------- Receiving data from the view -------------------
+      success: function (data) { // if send successful
+        data = data
+      },
+      failure: function () {
+          alert("failure");
+      }
+  })
+}
 
